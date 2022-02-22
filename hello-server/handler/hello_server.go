50df78ae4036adc6b5d706215b3a14b753a51081
Srijan Rastogi <srijan.rastogi@zopsmart.com>
2022-02-22 14:51:51 +0530

    handler function for the server

---
+++ hello-server/handler/hello_server.go
@@ -0,0 +1,34 @@
+package handler
+
+import (
+	"net/http"
+)
+
+// Hello function is the handler function for the hello-server and checks URL for different cases to write the desired outcome
+func Hello(w http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		_, _ = w.Write([]byte("Only get method is allowed!"))
+
+		return
+	}
+
+	params := req.URL.Query()
+
+	if len(params) > 0 {
+		name := params.Get("name")
+		if name == "" {
+			w.WriteHeader(http.StatusBadRequest)
+			_, _ = w.Write([]byte("Missing name parameter!"))
+
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+		_, _ = w.Write([]byte("Hello " + name))
+
+		return
+	}
+
+	_, _ = w.Write([]byte("hello"))
+}
